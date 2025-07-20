e47e2d7338dd03b669935d8213a554a171a89465
Santhyago Souza <santhyagoponciano@gmail.com>
2025-07-20 01:17:55 -0300

    implementacao de funcoes separadas para calculos repetitivos criando reaproveitamento de codigo.

--- js/script.js
+++ js/script.js
@@ -16,44 +16,63 @@ document.getElementById("cst_icms").addEventListener("input", async () => {
 
 
 
+
+
 function calcularIcms() {
 
   const cst = document.querySelector("#cst_icms").value;
-
-  let baseIcms = parseFloat(document.getElementById("base_calculo_icms").value);
-  let aliquotaIcms = document.getElementById("aliquota_icms").value;
-  let valor_icms = 0;
-
-  let baseIcmsSub = document.querySelector('#base_calculo_sub').value;
-  let aliquotaSubIcms = document.querySelector('#aliquota_sub').value;
-  let valorIcmsSub = 0;
+  const baseIcmsEl = document.getElementById("base_calculo_icms");
+  const aliquotaIcmsEl = document.getElementById("aliquota_icms");
 
 
 
+  let aliquotaSubIcmsEl = document.querySelector('#aliquota_sub');
+  let freteEl = document.querySelector('#valor_frete');
+  let mvaEl = document.querySelector('#valor_mva');
+  let reducaoIcmsEl = document.querySelector('#reducao');
+  let aliquotaIcmsSub = 0;
+  let frete = 0;
+  let mva = 0;
+  let baseIcms = 0;
+  let aliquotaIcms = 0;
 
 
+  if (baseIcmsEl) {
+    baseIcms = parseFloat(baseIcmsEl.value || 0);
+  }
+  if (aliquotaIcmsEl) {
+    aliquotaIcms = parseFloat(aliquotaIcmsEl.value || 0);
+  }
+  if (aliquotaSubIcmsEl) {
+    aliquotaIcmsSub = parseFloat(aliquotaSubIcmsEl.value || 0);
+  }
+  if (freteEl) {
+    frete = parseFloat(freteEl.value || 0);
+  }
 
+  if (mvaEl) {
+    mva = parseFloat(mvaEl.value || 0);
+  }
+  if (reducaoIcmsEl) {
+    reducao = parseFloat(reducaoIcmsEl.value || 0);
+  }
 
   switch (cst) {
     case "00":
-      valor_icms = (baseIcms * (aliquotaIcms / 100)).toFixed(2);
-      console.log(valor_icms);
-      document.getElementById("valor_icms").innerHTML = valor_icms;
+      calcularIcmsProprio(baseIcms, aliquotaIcms);
 
       break;
     case "10":
-      valor_icms = (baseIcms * (aliquotaIcms / 100)).toFixed(2);
-      console.log(valor_icms);
-      document.getElementById("valor_icms").innerHTML = valor_icms;
-
-      valorIcmsSub = (baseIcmsSub * (aliquotaSubIcms / 100)).toFixed(2);
-      console.log(valor_icms);
-      document.getElementById("valor_sub_icms").innerHTML = valorIcmsSub;
+      calcularIcmsProprio(baseIcms, aliquotaIcms);
+      calcularIcmsSubstituto(baseIcms, aliquotaIcmsSub, frete, mva)
       break;
 
     case "20":
-      let reducaoIcms = document.querySelector("#reducao").value;
-      console.log(reducaoIcms)
+      calcularReducaoBaseIcms(baseIcms, aliquotaIcms, reducao)
+
+      break;
+    case "30":
+      calcularIcmsSubstituto(baseIcms, aliquotaIcmsSub, frete, mva)
 
       break;
 
@@ -61,4 +80,35 @@ function calcularIcms() {
       alert("Esse CST não existe ou não foi implementado ainda.");
       return;
   }
+}
+
+function calcularIcmsProprio(baseIcms, aliquotaIcms) {
+  valor_icms = (baseIcms * (aliquotaIcms / 100)).toFixed(2);
+
+  document.getElementById("valor_icms").innerHTML = valor_icms;
+  return valor_icms;
+}
+
+function calcularIcmsSubstituto(baseIcms, aliquotaIcmsSub, frete, mva) {
+
+  let baseIcmsSub = (baseIcms + frete) * (1 + (mva / 100));
+  console.log(baseIcmsSub);
+  document.getElementById("valor_base_icms_sub").innerHTML = baseIcmsSub.toFixed(2);
+  let valorIcmsSub = (baseIcmsSub * (aliquotaIcmsSub / 100)).toFixed(2);
+  console.log(valorIcmsSub);
+  document.getElementById("valor_sub_icms").innerHTML = valorIcmsSub;
+  return valorIcmsSub, baseIcmsSub;
+}
+
+function calcularReducaoBaseIcms(baseIcms, aliquotaIcms, reducao) {
+  const baseReduzida = (baseIcms * (1 - (reducao / 100))).toFixed(2);
+  valor_icms = (baseReduzida * (aliquotaIcms / 100)).toFixed(2);
+  console.log("Base Reduzida:", baseReduzida);
+  console.log("ICMS com redução:", valor_icms);
+
+  document.getElementById("valor_base_reduzida").innerHTML = baseReduzida;
+  document.getElementById("valor_icms").innerHTML = valor_icms;
+  console.log(valor_icms);
+
+  return valor_icms;
 }
